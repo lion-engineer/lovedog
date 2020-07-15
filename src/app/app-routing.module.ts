f115a6d812eb4e1654d26869a9c1197a24f79d08
LeoLin <leo800807@gmail.com>
2020-07-15 17:53:18 +0800

    調整BANNER

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch:'full' },
-  { path: '**', redirectTo: 'index' }
+  { path: '', redirectTo: '/index', pathMatch:'full' },
+  { path: '**', redirectTo: '/index' }
 ];
 
 @NgModule({
